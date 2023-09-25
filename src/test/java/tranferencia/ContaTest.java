2bbbbaef2afea605d95c25fefa635e62711ebbaa
Henrique Bastos <henriquebastos46@gmail.com>
2023-09-25 16:23:26 -0300

    feat-criacao do projeto criacao

---
+++ src/test/java/tranferencia/ContaTest.java
@@ -0,0 +1,27 @@
+package tranferencia;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ContaTest{
+
+    @Test
+
+
+    public void realizarTransacao(){
+        Cliente xuxa = new Cliente("xuxa","123456789", "111111");
+        Cliente silvioSantos = new Cliente("Silvio Santos", "00987654321", "22222");
+
+        Conta contaXuxa = new Conta("0025", "2254", 2500.00, xuxa);{
+        Conta contaSilvio = new Conta("0026", "2251", 3500.00, silvioSantos);
+
+        contaXuxa.realizarTransferencia(1000.00, contaSilvio);
+        assertEquals( 1500.00, contaXuxa.getSaldo());
+        assertEquals( 4500.00, contaSilvio.getSaldo());
+            }
+
+
+    }
+
+}
